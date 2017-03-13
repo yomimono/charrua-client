c76e5a3845432a8466a0824b30e8b0d964aa139b
Mindy Preston <mindy.preston@docker.com>
2017-03-13 09:08:13 -0500

    send dhcprequests for our lease at t1
    In the Mirage DHCP client, take note of the RENEWAL_T1 option when
    entering the BOUND state, and send a DHCPREQUEST for the obtained lease
    after that time.
    
    As we can't call Net.listen without displacing callbacks the application has
    likely registered, it's not possible to hear any response to this
    DHCPREQUEST.  Without some refactoring, it won't be possible
    for the DHCP client here to hear a new lease, acknowledge a server's
    instruction that it not continue to use the lease it originally
    obtained, or move into the REBINDING state as demanded by the RFC.
    
    This patch simply sends the DHCPREQUEST messages as the RFC specifies
    in hopes that they may result in the existing lease being retained for longer.

--- src/mirage/dhcp_client_mirage.ml
+++ src/mirage/dhcp_client_mirage.ml
@@ -1,29 +1,40 @@
-let ipv4_config_of_lease lease : Mirage_protocols_lwt.ipv4_config option =
+let config_of_lease lease : (int32 * int32 * Mirage_protocols_lwt.ipv4_config) option =
   let open Dhcp_wire in
+  let expiration lease =
+    let t1 = match Dhcp_wire.find_renewal_t1 lease with
+    | Some n -> n | None -> 1800l (* 30 minutes *)
+    in
+    let t2 = match Dhcp_wire.find_rebinding_t2 lease with
+    | Some n -> n | None -> 3600l (* 1 hour *)
+    in
+    (t1, t2)
+  in
   (* ipv4_config expects a single IP address and the information
    * needed to construct a prefix.  It can optionally use one router. *)
   let address = lease.yiaddr in
   match Dhcp_wire.find_subnet_mask lease.options with
   | None -> None
   | Some subnet ->
+    let t1, t2 = expiration lease.options in
     let network = Ipaddr.V4.Prefix.of_netmask subnet address in
     let valid_routers = Dhcp_wire.collect_routers lease.options in
     match valid_routers with
-    | [] -> Some (Mirage_protocols_lwt.{ address; network; gateway = None })
+    | [] -> Some (t1, t2, Mirage_protocols_lwt.{ address; network; gateway = None })
     | hd::_ ->
-      Some (Mirage_protocols_lwt.{ address; network; gateway = (Some hd) })
+      Some (t1, t2, Mirage_protocols_lwt.{ address; network; gateway = (Some hd) })
 
 let src = Logs.Src.create "dhcp_client"
 module Log = (val Logs.src_log src : Logs.LOG)
 
 module Make(Time : Mirage_types_lwt.TIME) (Net : Mirage_types_lwt.NETWORK) = struct
   open Lwt.Infix
+  open Mirage_protocols_lwt
 
-  type t = Mirage_protocols_lwt.ipv4_config Lwt_stream.t
+  type t = ipv4_config Lwt_stream.t
 
   let usable_config_of_lease = function
   | None -> None
-  | Some lease -> ipv4_config_of_lease lease
+  | Some lease -> config_of_lease lease
 
   let connect ?(requests : Dhcp_wire.option_code list option) net =
     (* listener needs to occasionally check to see whether the state has advanced,
@@ -33,22 +44,46 @@ module Make(Time : Mirage_types_lwt.TIME) (Net : Mirage_types_lwt.NETWORK) = str
     let (client, dhcpdiscover) = Dhcp_client.create ?requests (Net.mac net) in
     let c = ref client in
 
-    let rec repeater dhcpdiscover =
+    (* if the lease specifies an expiration time, sleep that long and then
+     * try renewing.
+     * Note that this will only *send* packets, and in the context of a
+     * Mirage application which has already started with the lease configuration,
+     * attempting to spy on the listen loop to hear any replies would
+     * disrupt the user application (as calls to `Net.listen` overwrite any
+     * previous listener).  Therefore it's not currently possible to hear
+     * any reply to the request for renewal, be it positive or negative. *)
+
+    (* this logic is also not quite right for the case where we can
+     * do something about a failure to bind -- what we should actually do is
+     * also check the t2 timer and then start a new full lease transaction,
+     * but that's not possible for us for the reasons above. *)
+    let rec renew c t =
+      Time.sleep_ns @@ Duration.of_sec t >>= fun () ->
+      match Dhcp_client.renew c with
+      | None -> Log.debug (fun f -> f "Don't need to renew the lease; won't try");  Lwt.return_unit
+      | Some (c, buf) ->
+        Log.debug (fun f -> f "attempted to renew lease: %a" Dhcp_client.pp c);
+        Net.write net buf >|= Rresult.R.get_ok >>= fun () ->
+        renew c t (* ideally t would come from the new lease... *)
+    in
+    let rec get_lease dhcpdiscover =
       Log.debug (fun f -> f "Sending DHCPDISCOVER...");
       Net.write net dhcpdiscover >|= Rresult.R.get_ok >>= fun () ->
       Time.sleep_ns sleep_interval >>= fun () ->
       match usable_config_of_lease (Dhcp_client.lease !c) with
-      | Some lease ->
-        Log.info (fun f -> f "Lease obtained! IP %a network %a gateway %a"
-          Ipaddr.V4.pp_hum lease.address Ipaddr.V4.Prefix.pp_hum lease.network
-          (Fmt.option Ipaddr.V4.pp_hum) lease.gateway);
-          Lwt.return (Some lease)
+      | Some (t1, _t2, config) ->
+        Log.info (fun f -> f "Lease obtained! Renewal will begin in %lu seconds. IP: %a, network: %a, gateway: %a"
+          t1 Ipaddr.V4.pp_hum config.address Ipaddr.V4.Prefix.pp_hum config.network
+          (Fmt.option Ipaddr.V4.pp_hum) config.gateway);
+          (* to keep the lease valid, send renewal messages *)
+          Lwt.async (fun () -> renew !c @@ Int32.to_int t1);
+          Lwt.return (Some config)
       | None ->
         let (client, dhcpdiscover) = Dhcp_client.create ?requests (Net.mac net) in
         c := client;
         Log.info (fun f -> f "Timeout expired without a usable lease!  Starting over...");
         Log.debug (fun f -> f "New lease attempt: %a" Dhcp_client.pp !c);
-        repeater dhcpdiscover 
+        get_lease dhcpdiscover 
     in
     let listen () =
       Net.listen net (fun buf ->
@@ -62,11 +97,13 @@ module Make(Time : Mirage_types_lwt.TIME) (Net : Mirage_types_lwt.NETWORK) = str
           c := s; Lwt.return_unit
       ) >|= Rresult.R.get_ok
     in
-    let get_lease () =
-      Lwt.pick [ (listen () >>= fun () -> Lwt.return None);
-               repeater dhcpdiscover; ]
+    let lease_wrapper () =
+      Lwt.pick [
+        (listen () >>= fun () -> Lwt.return None); (* if canceled, the stream should end *)
+        get_lease dhcpdiscover; (* will terminate once a lease is obtained *)
+      ]
     in
-    let s = Lwt_stream.from get_lease in
+    let s = Lwt_stream.from lease_wrapper in
     Lwt.return s
 
 end
